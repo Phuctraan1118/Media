0c046c70bba19e0823d1f3180e696eb6015161d9
Phuc Tran <thienphucrt@gmail.com>
2024-10-21 00:03:58 +0700

    commit

---
+++ index.js
@@ -0,0 +1,51 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const { sendToQueue, getMessage } = require("./utils");
+const app = express();
+
+// Middleware
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/live-stream', express.static(path.join(__dirname, 'live-stream')));
+// Log API requests
+app.use((req, res, next) => {
+  console.log(req.method + " " + req.path);
+  next();
+});
+
+app.post("/api/webhooks/cloudflare", async (req, res) => {
+  const data = req.body;
+  const queueName = `bunny_livestream_${process.env.RABBITMQ_PREFIX}`;
+
+  try {
+    await sendToQueue(queueName, data);
+
+    res.status(200).json({ message: "OK" });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to process webhook" });
+  }
+})
+
+// getMessage(`bunny_livestream_${process.env.RABBITMQ_PREFIX}`);
+
+// Start server
+const port = process.env.DEVELOPMENT_PORT || 3000;
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log("Failed to start server:", err);
+    process.exit(1);
+  } else {
+    console.log(`Server is running at: http://localhost:${port}`);
+  }
+});
---
+++ utils.js
@@ -0,0 +1,97 @@
+const amqp = require("amqplib");
+
+// Hàng đợi để xử lý chuyển đổi và upload
+const startProcessingQueueBunny = async (socketNamespace, userSocketMap) => {
+    try {
+        const connection = await amqp.connect({
+            protocol: 'amqp',
+            hostname: process.env.RABBITMQ_URL,
+            username: process.env.RABBITMQ_USER,
+            password: process.env.RABBITMQ_PASS,
+        });
+
+        const channel = await connection.createChannel();
+        const queue = `video_processing_bunny${process.env.RABBITMQ_PREFIX}`;
+
+        await channel.assertQueue(queue, { durable: true });
+        const concurrentTasks = 10;
+        channel.prefetch(concurrentTasks);
+
+        channel.consume(queue, async (msg) => {
+            if (msg !== null) {
+                const message = JSON.parse(msg.content.toString());
+                const { videoUrl, m3u8Path, userId } = message;
+
+                const fileNameWithExtension = path.basename(videoUrl); 
+
+                const videoId = path.parse(fileNameWithExtension).name;
+
+                console.log('Processing video for userId:', videoUrl);
+
+                const socketId = userSocketMap.get(userId); 
+
+                const videoFilePath = path.resolve(`./uploads/${userId}/raw/`, path.basename(videoId));
+                const outputDir = path.resolve(`./uploads/${m3u8Path}`);
+                const outputFileName = `${path.basename(videoFilePath, path.extname(videoFilePath))}.m3u8`;
+
+                fs.mkdirSync(outputDir, { recursive: true });
+
+                try {
+                    await convertVideoToM3U8(videoUrl, outputDir, outputFileName);
+                    await uploadDirectoryToBunnyCDN(outputDir, userId, videoId, socketNamespace, socketId, m3u8Path);
+
+                    try {
+
+                        console.log("delete file m3u8", outputDir);
+                        // Xóa tất cả tệp trong thư mục
+                        fs.rmSync(outputDir, { recursive: true, force: true });
+                        console.log("delete file gốc", outputDir);
+                        deleteFolderContents(`./uploads/${userId}/raw/`);
+                        console.log("delete file done");
+                        // Gửi sự kiện cho client có userId
+                        // socketNamespace.to(socketId).emit('videoUploadComplete', { videoId: outputFileName, userId });
+                    } finally {
+                        channel.ack(msg);
+                        console.log("queue channel.ack(msg);");
+
+                    }
+                } catch (err) {
+                    console.error('Failed to process video:', err);
+                }
+
+            }
+        });
+    } catch (error) {
+        console.error('Failed to start processing queue:', error);
+    }
+};
+
+const rabbitMQUrl = `amqp://livestream_1:DMCF5qyDg6wx2g3m8n@62.77.156.171`;
+
+// Helper function to send message to RabbitMQ
+async function sendToQueue(queueName, message) {
+  try {
+    const connection = await amqp.connect(rabbitMQUrl);
+    const channel = await connection.createChannel();
+
+    // Ensure the queue exists
+    await channel.assertQueue(queueName, { durable: true });
+
+    // Send the message
+    channel.sendToQueue(queueName, Buffer.from(JSON.stringify(message)), {
+      persistent: true,
+    });
+
+    console.log(`[x] Sent to queue ${queueName}:`, message);
+
+    // Close the connection after a short delay
+    setTimeout(() => {
+      channel.close();
+      connection.close();
+    }, 500);
+  } catch (error) {
+    console.error("Error sending to RabbitMQ:", error);
+  }
+}
+
+module.exports = { sendToQueue };
